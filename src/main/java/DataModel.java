b828d2e96d5a7d449c1a1ba1358e02f9cd5bbfce
grim13b <grim13b@users.noreply.github.com>
2016-08-24 01:43:39 +0900

    Fix warning

--- src/main/java/DataModel.java
+++ src/main/java/DataModel.java
@@ -1,15 +1,10 @@
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
 public class DataModel {
-
-    @NonNull
     private int id;
 
-    @NonNull
     private String description;
-
 }
